3da8fe7bfcb4997e1990a25f4f655275c4a57b56
jsully3 <jsully3@outlook.com>
2021-03-11 18:30:58 -0500

    added todos

--- Crypto_Tradrr/ui/views.py
+++ Crypto_Tradrr/ui/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(response):
     return render(response, 'base.html')
+
+# todo: setup views for other pages
+
+# todo: layout content for dashboard
