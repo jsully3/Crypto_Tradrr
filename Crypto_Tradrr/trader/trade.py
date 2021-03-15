2a16e10608b421bc30175a6f9ceac404739d15f9
jsully3 <jsully3@outlook.com>
2021-03-15 13:14:01 -0400

    started trade algo

--- Crypto_Tradrr/trader/trade.py
+++ Crypto_Tradrr/trader/trade.py
@@ -1,1 +1,24 @@
 # todo: come up with trading algo
+
+from datetime import datetime
+
+
+def current_time():
+    # datetime object containing current date and time
+    now = datetime.now().strftime('%m-%d-%Y %H:%M:%S')
+    return str(now)
+
+
+print("Enter the Currency rate: ")
+currency = float(input())
+
+
+def trade_algo():
+    if currency > 5:
+        return print("Sell Sell Sell")
+    if currency < 1:
+        return print("Buy Buy Buy")
+    else:
+        return print("Stay In")
+
+trade_algo()
